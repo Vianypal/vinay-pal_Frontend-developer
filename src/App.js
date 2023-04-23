66322d826a5e4a6e315a703dade9f78f80e5bca7
vinay pal <vinaypal.knp01@gmail.com>
2023-04-23 21:24:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+const items = [
+{ text: 'Item 1' },
+{ text: 'Item 2' },
+{ text: 'Item 3' },
+];
+
+return (
+<div>
+<h1>List Component Demo</h1>
+<List items={items} />
+</div>
+);
+};
 
 export default App;
